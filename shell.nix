e9777aaf88a9c6c68aa00c843501482fff9c4b26
firefenix <kitotavrik.media@gmail.com>
2024-08-04 01:02:10 +0300

    Merge fixing

--- shell.nix
+++ shell.nix
@@ -1,15 +1,13 @@
 let
 	pkgs = import <nixpkgs> {};
+
 in pkgs.mkShell {
 	packages = [
 		(pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
 			requests
 			beautifulsoup4
-<<<<<<< HEAD
 			click
-=======
 			requests-cache
->>>>>>> origin/master
 		]))
 		pkgs.clang-tools
 	];
